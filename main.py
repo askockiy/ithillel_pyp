e8e367492496456aefa24d3f5826f4565f717c38
Alexander.Skockiy <alexander.skockiy@scrdairy.com>
2022-12-30 13:13:57 +0200

    tests

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from urllib.parse import urlsplit, parse_qs, parse_qsl
+from urllib.parse import urlsplit, parse_qsl
 
 def parse(query: str) -> dict:
     return dict((k, v if len(v)>1 else v[0]) for k, v in parse_qs(urlsplit(query).query).items())
@@ -21,3 +21,14 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('i=00;p=***&^%') == {'id': '00', 'p': '***&^%'}
+    assert parse_cookie('i=01;;;;p=!@\/\/\//\///') == {'id': '01', 'p': '!@\\/\\/\\//\\///'}
+    assert parse_cookie(';;;;;;') == {}
+    assert parse_cookie('user=asd;;lastn=ddd;ad=vvv') == {'user': 'asd', 'lastn': 'ddd', 'ad': 'vvv'}
+    assert parse_cookie('d=1;d=2') == {'d': '2'}
+    assert parse_cookie('1=1;2=2') == {'1': '1', '2': '2'}
+    assert parse_cookie('io======;d') == {'io': '====='}
+    assert parse_cookie('fghjkl=ddf;;;') == {'fghjkl': 'ddf'}
+    assert parse_cookie('!@$%^&*()_+=0000') == {'!@$%^&*()_ ': '0000'}
+    assert parse_cookie('--;--') == {}
+    
